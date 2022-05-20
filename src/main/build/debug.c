pmattila/scratchpad

RTFL: Add DEBUG32 32bit debug variable support
If USE_DEBUG32 is defined in target.h, extra 8 debug
variables will be available, with DEBUG32_SET() macro.

Does not require any changes to the blackbox explorer!

By default these variables will show up as debug32[]
in blackbox explorer, but they can be renamed by
modifying src/main/blackbox/blackbox.c file.

It's also possible to have more than 8 variables, but
then the definitions in blackbox.c MUST be modified, as
blackbox arrays are limited to size of 8.

src/main/build/debug.c
@@ -25,6 +25,10 @@
 FAST_RAM_ZERO_INIT uint8_t debugMode;
 FAST_RAM_ZERO_INIT int16_t debug[DEBUG16_VALUE_COUNT];
 
+#ifdef USE_DEBUG32
+FAST_RAM_ZERO_INIT int32_t debug32[DEBUG32_VALUE_COUNT];
+#endif
+
 #ifdef DEBUG_SECTION_TIMES
 uint32_t sectionTimes[2][4];
 #endif

src/main/build/debug.h
@@ -23,11 +23,20 @@
 #include "platform.h"
 
 #define DEBUG16_VALUE_COUNT 4
+#define DEBUG32_VALUE_COUNT 8
 
 extern FAST_RAM_ZERO_INIT uint8_t debugMode;
 extern FAST_RAM_ZERO_INIT int16_t debug[DEBUG16_VALUE_COUNT];
 
+#ifdef USE_DEBUG32
+extern FAST_RAM_ZERO_INIT int32_t debug32[DEBUG32_VALUE_COUNT];
+#endif
+
 #define DEBUG_SET(mode, index, value)   do { if (debugMode == (mode)) debug[(index)] = (value); } while(0)
+
+#ifdef USE_DEBUG32
+#define DEBUG32_SET(mode, index, value)   do { if (debugMode == (mode)) debug32[(index)] = (value); } while(0)
+#endif
 
 #define DEBUG_SECTION_TIMES
 

Jay-C/rotorflight-firmware

src/main/build/debug.c
@@ -25,6 +25,10 @@
 FAST_DATA_ZERO_INIT uint8_t debugMode;
 FAST_DATA_ZERO_INIT int16_t debug[DEBUG16_VALUE_COUNT];
 
+#ifdef USE_DEBUG32
+FAST_DATA_ZERO_INIT int32_t debug32[DEBUG32_VALUE_COUNT];
+#endif
+
 #define DEBUG_NAME(x)  [DEBUG_ ## x] = #x
 
 // Please ensure that names listed here match the enum values defined in 'debug.h'

src/main/build/debug.h
@@ -23,11 +23,22 @@
 #include "platform.h"
 
 #define DEBUG16_VALUE_COUNT 4
+#define DEBUG32_VALUE_COUNT 8
 
 extern FAST_DATA_ZERO_INIT uint8_t debugMode;
 extern FAST_DATA_ZERO_INIT int16_t debug[DEBUG16_VALUE_COUNT];
 
+#ifdef USE_DEBUG32
+extern FAST_DATA_ZERO_INIT int32_t debug32[DEBUG32_VALUE_COUNT];
+#endif
+
 #define DEBUG_SET(mode, index, value) do { if (debugMode == (mode)) { debug[(index)] = (value); } } while (0)
+
+#ifdef USE_DEBUG32
+#define DEBUG32_SET(mode, index, value) do { if (debugMode == (mode) && (index) < DEBUG32_VALUE_COUNT) { debug32[(index)] = (value); } } while (0)
+#else
+#define DEBUG32_SET(mode, index, value) (void)
+#endif
 
 typedef enum {
     DEBUG_NONE,